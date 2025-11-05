289a09282d9c469979c5da0f8b0d06037e5e3504
CesValde <valderramacesar2002@gmail.com>
2025-11-05 18:22:01 -0300

    Dependencias

---
+++ Backend_I/Semana6/src/public/js/index.js
@@ -0,0 +1,54 @@
+/* console.log("esto deberia de verse en consola - pantalla socket") */
+
+const socket = io(); // establece la conexion del cliente hacia el server 
+const box = document.querySelector('#box')          // input
+const app = document.querySelector('#app-chat')     // div con los <p> mensajes <p>
+const teclaEnter = 'Enter'
+/* const userStatus = document.querySelector() */
+
+let user = '';
+
+// oculto el div app, hasta que el usuario no valide
+app.style.display = 'none'
+
+// mensaje de bienvenida
+Swal.fire({
+    title: 'Quien sos?',
+    input: 'text',
+    text: 'Ingresa un nick para identificarte en la sala',
+    allowOutsideClick: false,
+    inputValidator: (value) => {
+        return !value && 'necesitas un nick para poder chatear'
+    } 
+}).then(nick => {
+    user = nick.value
+    alert(`Bienvenido ${user}`)
+    socket.emit('registrar_usuario', user)
+    app.style.display = 'block'
+})
+
+// 
+box.addEventListener('keyup', (e) => {
+    const { key, target } = e
+    if(key === teclaEnter && target.value !== ''){
+        // enviar al servidor el mensaje con el usuario
+        
+        socket.emit('mensaje',{ user, mensaje: target.value })
+        box.value = '';
+    }
+})
+
+// itera cada mensaje 
+socket.on('lista_de_mensaje_actualizada', (data) => {
+    // cree la card o la fila
+    app.innerHTML = ''
+    data.forEach(chat => {
+        if(user == chat.user) { chat.user = 'Yo'} else { chat.user = chat.user }
+
+        const p = document.createElement('p')
+        p.innerText = `${chat.user}: ${chat.mensaje}` 
+        app.appendChild(p)
+
+    })
+})
+
---
+++ Backend_I/Semana6/src/routes/view.route.js
@@ -0,0 +1,19 @@
+import { Router } from "express"; // export comun sin default
+const route = Router();
+ 
+route.get('/notas', (req, res) => {
+    res.render('perfil', {
+        name: "Pedro sanchez",
+        rol: "guest",
+        isAdmin: true,
+        notas: [{curso: "javascript", nota:10},{curso: 'html', nota:9}, {curso:'css', nota:6}, {curso:'react', nota: 3}]
+    }) 
+    // primer parametro -> define la vista index.handlebars
+    // segundo parametro -> data que va a recibir la vista
+})
+
+route.get('/socket', (req, res) => {
+    res.render('chat',{})
+})
+
+export default route;
